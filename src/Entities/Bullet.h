4e0a77f98eb819b566bc543dd42f82d9362ce6fb
Adrian Rojas <arojasgomez.93@gmail.com>
2024-11-04 09:21:01 -0300

    GameFeel05: Bullet Spread

--- src/Entities/Bullet.h
+++ src/Entities/Bullet.h
@@ -1,42 +1,52 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
-
+#include <random>
 
 class Bullet
 {
-
 public:
-	Bullet();
-	~Bullet();
-	void Update();
-	void Draw(sf::RenderWindow& window);
-	void Fire(float posX, float posY, float dirX, float dirY);
-	int GetDamage() { return damage; }
-	bool GetIsActive() { return isActive; }
-	sf::Sprite GetBulletSprite() { return bulletSprite; }
-	void SetBulletSprite(bool isActive);
+    Bullet();
+    ~Bullet();
+    void Update();
+    void Draw(sf::RenderWindow& window);
+    void Fire(float posX, float posY, float dirX, float dirY);
+    int GetDamage() { return damage; }
+    bool GetIsActive() { return isActive; }
+    sf::Sprite GetBulletSprite() { return bulletSprite; }
+    void SetBulletSprite(bool isActive);
 
 private:
-	sf::Texture* bulletTexture;
-	sf::Sprite bulletSprite;
-	sf::Clock* timer;
-	
-	const float BULLET_LIFETIME = 0.65f;
-	float initialBulletSpeed = 750.0f;
-	float bulletSpeed = 650.0f;
-	bool isActive = false;
-	int damage = 10;
-	float scaleX = 0.25f;
-	float scaleY = 0.5f;
-	float posX = 0.0f;
-	float posY = 0.0f;
-	float bulletDirX = 0.0f;
-	float bulletDirY = 0.0f;
+    sf::Texture* bulletTexture;
+    sf::Sprite bulletSprite;
+    sf::Clock* timer;
 
-	void SetTextureValues();
-	void BulletMovement();
-	void BulletTimer();
-	void SetRotation();
-	void SetPosition();
+    const float BULLET_LIFETIME = 0.65f;
+    const float MAX_SPREAD_ANGLE = 5.0f; 
+    float initialBulletSpeed = 750.0f;
+    float bulletSpeed = 650.0f;
+    bool isActive = false;
+    int damage = 10;
+    float scaleX = 0.25f;
+    float scaleY = 0.5f;
+    float posX = 0.0f;
+    float posY = 0.0f;
+    float bulletDirX = 0.0f;
+    float bulletDirY = 0.0f;
+
+    void SetTextureValues();
+    void BulletMovement();
+    void BulletTimer();
+    void SetRotation();
+    void SetPosition();
+
+    // Random number generator components
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_real_distribution<float> dis;
+
+    // Helper functions
+    float getRandomDeviation();
+    float degToRad(float degrees);
+    sf::Vector2f rotateVector(const sf::Vector2f& vec, float angleRadians);
 };
