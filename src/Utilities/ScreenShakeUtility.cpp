d3c29826fb910d862c1a56897d05dd38ffb69dff
Adrian Rojas <arojasgomez.93@gmail.com>
2024-11-05 15:18:59 -0300

    Shake Completed!

--- src/Utilities/ScreenShakeUtility.cpp
+++ src/Utilities/ScreenShakeUtility.cpp
@@ -3,10 +3,10 @@
 
 ScreenShake::ScreenShake(sf::View* viewCenter)
 {
-	this->viewCenter = *viewCenter;
-	this->view = *viewCenter;
-	generator = std::mt19937(rd());
-	distribution = std::uniform_real_distribution<float>(-1.0f, 1.0f);
+    this->viewCenter = *viewCenter;
+    this->view = *viewCenter;
+    generator = std::mt19937(rd());
+    distribution = std::uniform_real_distribution<float>(-1.0f, 1.0f);
 
 }
 
@@ -16,36 +16,35 @@ ScreenShake::~ScreenShake()
 
 void ScreenShake::Update()
 {
-	if (!isShaking)
-		return;
+    if (!isShaking)
+        return;
 
-	if (shakeClock.getElapsedTime().asSeconds() > shakeDuration)
-	{
-		isShaking = false;
-		view = viewCenter;
-		return;
-	}
+    if (shakeClock.getElapsedTime().asSeconds() > shakeDuration)
+    {
+        isShaking = false;
+        return;
+    }
 
-	float offsetX = distribution(generator) * shakeIntensity;
-	float offsetY = distribution(generator) * shakeIntensity;
+    float offsetX = distribution(generator) * shakeIntensity;
+    float offsetY = distribution(generator) * shakeIntensity;
 
-	view.move(viewCenter.getCenter().x + offsetX, viewCenter.getCenter().y + offsetY);
+    view.setCenter(viewCenter.getCenter().x + offsetX, viewCenter.getCenter().y + offsetY);
 }
 
 void ScreenShake::Draw(sf::RenderWindow& window)
 {
-	if (isShaking)
-		window.setView(view);
-	else
-		window.setView(viewCenter);
+    if (isShaking)
+        window.setView(view);
+    else
+        window.setView(viewCenter);
 }
 
 void ScreenShake::StartShake(float shakeDuration, float shakeIntensity)
 {
-	this->shakeDuration = shakeDuration;
-	this->shakeIntensity = shakeIntensity;
-	isShaking = true;
-	shakeClock.restart();
-	view = viewCenter;
-
+    this->shakeDuration = shakeDuration;
+    this->shakeIntensity = shakeIntensity;
+    isShaking = true;
+    shakeClock.restart();
+    view = viewCenter;
 }
+
